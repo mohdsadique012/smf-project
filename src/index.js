55e25eedc010c5a8df53dd0da0c49462e0017f9b
mohdsadique012 <mohdsadique012000@gmail.com>
2023-01-08 17:21:19 +0530

    new

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   // <React.StrictMode>
     <BrowserRouter>
       {/* <App /> */}
-      <AppProvider>
+      <AppProvider >
         <App />
       </AppProvider>
     </BrowserRouter>
